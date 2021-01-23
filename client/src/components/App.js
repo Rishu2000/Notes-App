228736084e215dfe4b8371aa184c1ac0e9e36b2f
Your Name <you@example.com>
2021-01-22 21:07:28 -0600

    Created a state and some buttons to modify.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import Header from "./Header/Header";
 export default class App extends Component {
+state={
+  user:null
+}
   render() {
     return (
       <div className="App">
         <Header Dark={true} className="Header">App</Header>
+        <div className="row">
+          <div className="container">
+            <div className="col-12">
+        <p>{this.state.user?`User Loged In as ${this.state.user.name}`:"User Loged Out"}</p>
+        <button className="btn btn-primary" onClick={
+          (e) => {
+            e.preventDefault();
+            this.setState({
+              user:{name:"Rishav"}
+            })
+          }
+        }>
+          LogIn
+        </button>
+        <button className="btn btn-secondary ml-2" onClick={
+          (e) => {
+            e.preventDefault();
+            this.setState({user:null})
+          }
+        }>LogOut</button>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }

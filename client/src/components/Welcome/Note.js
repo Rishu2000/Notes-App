57d752cde5af65678b26ce47b7d3c4fea1bca903
Your Name <you@example.com>
2021-01-25 14:32:46 -0600

    Created Route for Note.

--- client/src/components/Welcome/Note.js
+++ client/src/components/Welcome/Note.js
@@ -1,9 +1,19 @@
 //import React from 'react';
+import {Switch,Route,withRouter} from "react-router-dom";
 
-const Note = ({currentNote}) => {
-  return currentNote !== null ?
-    "You are Lookin at Note no:- "+(currentNote+1):
-    "Click on left side."
+const Note = ({currentNote,...props}) => {
+  return (
+    <Switch>
+      <Route path="/" exact={true}>
+        Select something from left.
+      </Route>
+      <Route path="/:NoteID">
+        <pre className="border rounded bg-light">
+          {JSON.stringify(props,null,2)}
+        </pre>
+      </Route>
+    </Switch>
+  )
 }
 
 export default Note;

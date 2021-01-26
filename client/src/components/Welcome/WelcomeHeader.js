79115dea4b83a69a13ff5a3632be00e2b017a725
Your Name <you@example.com>
2021-01-26 07:26:33 -0600

    Added Link to Home Page.

--- client/src/components/Welcome/WelcomeHeader.js
+++ client/src/components/Welcome/WelcomeHeader.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
 
 const WelcomeHeader = ({user,handleLogOut}) => {
   return (
       <div className="row">
         <div className="col-12">
           <p>Welcome {user.Name}</p>
+          <Link to="/" className = "btn btn-primary mb-3 mr-3">Home</Link>
           <button className="btn btn-warning mb-3" onClick={handleLogOut}>Logout</button>
         </div>
       </div>
